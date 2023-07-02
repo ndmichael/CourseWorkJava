4eeb47d50c6c277201f33f27bc1677759848e145
ndmichael <ukejemicheal@gmail.com>
2023-07-02 15:18:36 +0100

    worked on the inputData method

--- CourseWork/src/Test.java
+++ CourseWork/src/Test.java
@@ -1,2 +1,68 @@
+import java.lang.reflect.Array;
+import java.util.Scanner;
+import java.util.ArrayList;
 public class Test {
+    public static void menu(ArrayList<Tweet> tweets){
+        boolean flag = true;
+        while (flag){
+            Scanner scan = new Scanner(System.in);
+            System.out.println("Enter 1 for input data \n"+ "Enter 2 for search data \n" + "press -1 to terminate.");
+            int choice = scan.nextInt();
+//            if (choice == -1){
+//                break;
+//            }
+            switch (choice){
+                case 1:
+                    inputData(tweets);
+                    break;
+                case 2:
+                    searchData();
+                    break;
+                case -1:
+                    System.out.println("Program terminated...");
+                    flag = false;
+                    break;
+
+                default:
+                    System.out.println("Wrong input.");
+            }
+        }
+
+    }
+
+    public static void inputData(ArrayList<Tweet> tweet){
+        int id, count;
+        String text = "";
+        String username = "";
+        Scanner s = new Scanner(System.in);
+        System.out.println("\n");
+        System.out.print("enter id: ");
+        id = s.nextInt();
+        System.out.print("enter likes: ");
+        count = s.nextInt();
+        System.out.print("enter username: ");
+        username = s.next();
+        System.out.print("enter tweet: ");
+        text = s.next();
+
+        Tweet obj = new Tweet(id, text, username, count);
+        tweet.add(obj);
+
+         tweet.get(0).print_data();
+
+    }
+
+    public static void searchData(){
+        System.out.println("Search database(array) by username.");
+    }
+
+    public static void printData(){
+
+    }
+
+    public static void main(String args []){
+        // set up the arrays of objects here
+        ArrayList<Tweet> tweets = new ArrayList<Tweet>(100);
+        menu(tweets);
+    }
 }
